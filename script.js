d02be6c9d538920168887c2023934c4ccfcee3b4
Hannah Schuster <hschuster9@gmail.com>
2017-06-23 19:10:43 -0400

    styling

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getWeather(){
         var widget = showResults(data);
 
         $('#showWeather').html(widget);
-        $(".icon").html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>")
+        $("#icon").html("<img style='height: 80px; margin: auto; display:block; margin-top: 10px;' class='weatherIcon' src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>")
         $('#city').val("");
 
       }
@@ -37,12 +37,12 @@ function showResults(data){
   var iconCode = data.weather[0].icon;
   var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
 
-  return "<h4>"+data.name+"</h4>"+
-          "<p>Condition: " +data.weather[0].description+"</p>"+
-          "<p>Temperature: "+data.main.temp+" &deg;F</p>"+
-          "<p>High: "+data.main.temp_max+" &deg;F</p>"+
-          "<p>Low: "+data.main.temp_min+" &deg;F</p>"+
-          "<p>Humidity: " +data.main.humidity+"%</p>"+
-          "<p>Wind: " +data.wind.speed+"mph</p>"
+  return "<h3 style='font-size:25px; padding-top:20px;' class='text-center'>"+data.name+"</h3>"+
+          "<p style='padding-left:40px;'><strong>Condition</strong>: "  +data.weather[0].description+"</p>"+
+          "<p style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;F</p>"+
+          "<p style='padding-left:40px;'><strong>High</strong>: "+data.main.temp_max+" &deg;F</p>"+
+          "<p style='padding-left:40px;'><strong>Low</strong>: "+data.main.temp_min+" &deg;F</p>"+
+          "<p style='padding-left:40px;'><strong>Humidity</strong>: " +data.main.humidity+"%</p>"+
+          "<p style='padding-left:40px; padding-bottom: 30px;'><strong>Wind</strong>: " +data.wind.speed+"mph</p>"
 
 }
