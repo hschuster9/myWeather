59f5c86b1c4a0685767b846760e45bb25fe3d6f0
Hannah Schuster <hschuster9@gmail.com>
2017-06-25 17:13:18 -0400

    changed api to weather underground

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ $(document).ready(function(){
 
 function getWeather(){
   var city = $("#city").val();
-  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=927340b14919bfd5a15c4c55b3888520`
+  var state = $("#state").val();
+  // var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=927340b14919bfd5a15c4c55b3888520`
+  var url = `https://api.wunderground.com/api/67e285089e02a77a/conditions/q/${state}/${city}.json`
+
+
 
   if(city != ""){
 
@@ -34,16 +38,19 @@ function getWeather(){
 
 //return value of parameter - temp
 function showResults(data){
-  var iconCode = data.weather[0].icon;
-  // var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-  var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+  // var iconCode = data.weather[0].icon;
+  // // var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+  // var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
 
-  return "<h3 style='font-size:25px; padding-top: 25px;' class='text-center'>"+data.name+"</h3>"+
-          "<p style='padding-left:40px; padding-top: 50px;'><strong>Condition</strong>: "  +data.weather[0].description+"</p>"+
-          "<p style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;F</p>"+
-          "<p style='padding-left:40px;'><strong>High</strong>: "+data.main.temp_max+" &deg;F</p>"+
-          "<p style='padding-left:40px;'><strong>Low</strong>: "+data.main.temp_min+" &deg;F</p>"+
-          "<p style='padding-left:40px;'><strong>Humidity</strong>: " +data.main.humidity+"%</p>"+
-          "<p style='padding-left:40px; padding-bottom: 30px;'><strong>Wind</strong>: " +data.wind.speed+"mph</p>"
+  // return "<h3 style='font-size:25px; padding-top: 25px;' class='text-center'>"+data.name+"</h3>"+
+  //         // "<p style='padding-left:40px; padding-top: 50px;'><strong>Condition</strong>: "  +data.weather[0].description+"</p>"+
+  //           "<p style='padding-left:40px; padding-top: 50px;'><strong>Condition</strong>: "  +data.weather[0].description+"</p>"+
+  //         "<p style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;F</p>"+
+  //         "<p style='padding-left:40px;'><strong>High</strong>: "+data.main.temp_max+" &deg;F</p>"+
+  //         "<p style='padding-left:40px;'><strong>Low</strong>: "+data.main.temp_min+" &deg;F</p>"+
+  //         "<p style='padding-left:40px;'><strong>Humidity</strong>: " +data.main.humidity+"%</p>"+
+  //         "<p style='padding-left:40px; padding-bottom: 30px;'><strong>Wind</strong>: " +data.wind.speed+"mph</p>"
+
+  return "<h3 style='font-size:25px; padding-top: 25px;' class='text-center'>"+data.current_observation.display_location.city+"</h3>"
 
 }
