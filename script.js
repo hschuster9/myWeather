4b477aeafec5a0ca174c311a23c967542beef16e
Hannah Schuster <hschuster9@gmail.com>
2017-06-27 13:58:56 -0400

    updated temp

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function showResults(data){
 
   return  "<h3 style='font-size:24px; padding-top: 35px;' class='text-center results'>"+data.current_observation.display_location.full+" ("+data.current_observation.display_location.zip+")</h3>"+
           "<img src="+data.current_observation.icon_url+" style='height: 50px; width: 50px; margin: auto; display:block;' class='results'>"+
-          "<p style='font-size: 40px; color: #FF7040'; class='text-center results'><strong>"+data.current_observation.temp_f+" &degF</strong> </p>"+
+          "<p style='font-size: 40px; color: #FF7040'; class='text-center results'><strong>"+data.current_observation.temp_f+"&degF</strong> </p>"+
           "<p style='padding-top: 22px;' class='text-center results'><strong>Condition</strong>: "+data.current_observation.weather+"</p>"+
           "<p class='text-center results'><strong>Humidity</strong>: "+data.current_observation.relative_humidity+"</p>"+
           "<p class='text-center results'><strong>Wind</strong>: "+data.current_observation.wind_mph+"mph, "+data.current_observation.wind_dir+"</p>"
